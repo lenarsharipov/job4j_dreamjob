e2f128c44f2062421668ba1901df59f89196508a
lenarsharipov <lenarsharipov@gmail.com>
2023-03-01 16:24:32 +0300

    1. Формы. Поля ввода. [#504853]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -20,17 +20,17 @@ public class MemoryVacancyRepository implements VacancyRepository {
 
     private MemoryVacancyRepository() {
         save(new Vacancy(0, "Intern Java Developer", "description of vacancy",
-                LocalDateTime.now()));
+                LocalDateTime.now(), false));
         save(new Vacancy(0, "Junior Java Developer", "description of vacancy",
-                LocalDateTime.now()));
+                LocalDateTime.now(), true));
         save(new Vacancy(0, "Junior+ Java Developer", "description of vacancy",
-                LocalDateTime.now()));
+                LocalDateTime.now(), false));
         save(new Vacancy(0, "Middle Java Developer", "description of vacancy",
-                LocalDateTime.now()));
+                LocalDateTime.now(), true));
         save(new Vacancy(0, "Middle+ Java Developer", "description of vacancy",
-                LocalDateTime.now()));
+                LocalDateTime.now(), false));
         save(new Vacancy(0, "Senior Java Developer", "description of vacancy",
-                LocalDateTime.now()));
+                LocalDateTime.now(), true));
     }
 
     @Override
@@ -49,10 +49,11 @@ public class MemoryVacancyRepository implements VacancyRepository {
     public boolean update(Vacancy vacancy) {
         return vacancies.computeIfPresent(
                 vacancy.getId(), (id, oldVacancy) -> new Vacancy(
-                        oldVacancy.getId(),
-                        vacancy.getTitle(),
-                        vacancy.getDescription(),
-                        vacancy.getCreationDate())
+                    oldVacancy.getId(),
+                    vacancy.getTitle(),
+                    vacancy.getDescription(),
+                    vacancy.getCreationDate(),
+                    vacancy.getVisible())
         ) != null;
     }
 
